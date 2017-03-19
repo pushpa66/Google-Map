088658ab0997ae1ef65c49621a3f69764553ff35
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-03-19 23:16:29 +0530

    start implementing get

--- app/src/main/java/com/example/pushpe/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/pushpe/googlemap/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.pushpe.googlemap;
 
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,12 +10,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.location.Address;
 
-//import com.google.android.gms.identity.intents.Address;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
@@ -23,6 +24,9 @@ import java.util.List;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker marker = null;
+    private Marker currentMarker = null;
+    EditText location_tf = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,27 +38,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         mMap.setMyLocationEnabled(true);
-    }
 
+    }
     public void onSearch(View view){
-        EditText location_tf = (EditText)findViewById(R.id.etAddress);
+        location_tf = (EditText)findViewById(R.id.etAddress);
         String location = location_tf.getText().toString();
         List<Address> addressList = null;
 
         if (location != null || !location.equals("")){
+
+            if(marker != null){
+                marker.remove();
+            }
             Geocoder geocoder = new Geocoder(this);
             try {
                 addressList = geocoder.getFromLocationName(location, 1);
@@ -62,9 +66,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 e.printStackTrace();
             }
         }
-        Address address = addressList.get(0);
-        LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        if(!addressList.isEmpty()){
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+            marker = mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14.0f));
+        }
+    }
+
+    public void changeType(View view){
+        if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL){
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        else {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
     }
 }
